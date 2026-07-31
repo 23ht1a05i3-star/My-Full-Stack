abcf20abb34da36e0820ef0ee69bf570dc4f97c0
Dinesh <23ht1a05i3@gmail.com>
2026-07-31 21:25:41 +0530

    checking  evenORodd

---
+++ evenORodd.py
@@ -0,0 +1,11 @@
+#fine the  user enter number is even or odd
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+#num = int(input("Enter a number: ")) this line is used to take  the input from the user
+#num%2==0 used tocheck the number is even or odd
+#if even print even number else oddnumber
