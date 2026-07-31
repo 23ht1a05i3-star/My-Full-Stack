74131a0f181ec981287b6024569f96a28d408d41
Dinesh <23ht1a05i3@gmail.com>
2026-07-31 21:14:51 +0530

    Added Python sum program

---
+++ sum.py
@@ -0,0 +1,15 @@
+#adding two float numbers with user given inputs
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum =", sum)
+
+#adiing two integer numbers with user inputs
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum =", sum)
